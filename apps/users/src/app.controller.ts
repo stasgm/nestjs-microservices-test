2cd4e85c268bddea4ed110411db59ebf31d2d257
Stanislau Shliakhtsich <stasgm@gmail.com>
2023-10-21 19:05:01 +0300

    Add Users microservice

--- apps/users/src/app.controller.ts
+++ apps/users/src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { delay } from 'rxjs';
+import { UserDTO } from './types';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'new_user' })
+  newUser(user: UserDTO): string {
+    delay(10000);
+    const result = this.appService.newUser(user);
+    if (!result) {
+      return 'Book already exists';
+    } else {
+      return result;
+    }
+  }
+
+  @MessagePattern({ cmd: 'get_user' })
+  getBook(userId: string): UserDTO {
+    return this.appService.getUserByID(userId);
+  }
+
+  @MessagePattern({ cmd: 'get_users' })
+  getBooks(): UserDTO[] {
+    return this.appService.getAllUsers();
   }
 }
--- apps/users/src/app.service.ts
+++ apps/users/src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
+import { UserDTO } from './types';
+
+const users: UserDTO[] = [];
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUserByID(userID: string) {
+    return users.find((b: UserDTO) => b.id == userID);
+  }
+
+  getAllUsers() {
+    return users;
+  }
+
+  newUser(user: UserDTO) {
+    const exists = users.find((usr: UserDTO) => {
+      return usr.name == user.name;
+    });
+
+    if (exists) {
+      return false;
+    }
+
+    user.id = 'User_' + (users.length + 1);
+    users.push(user);
+
+    return user.id;
   }
 }
